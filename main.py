36af95bd0fbb32f9d45da85edc8bece9af02d899
peet <petrounknown@gmail.com>
2025-01-19 14:34:32 +0300

    testing

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ model_path = os.path.join(os.path.dirname(__file__), 'RealESRGAN_x4plus.pth')
 
 state_dict=torch.load(model_path, map_location=torch.device("cpu"))["params_ema"]
 
-model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=10)
 model.load_state_dict(state_dict, strict=True)
 
 upsampler=RealESRGANer(
-    scale=4,
+    scale=10,
     model_path=model_path,
     model=model,
     tile=0,
@@ -26,13 +26,18 @@ upsampler=RealESRGANer(
     half=False
 )
 
-img = Image.open("image.jpg").convert("RGB")
+myImage=os.path.join(os.path.dirname(__file__), 'image.jpg')
+img = Image.open(myImage).convert("RGB")
 img = np.array(img)
 
-output, _ = upsampler.enhance(img, outscale=4)
+output, _ = upsampler.enhance(img, outscale=10)
 
+myOutput=os.path.join(os.path.dirname(__file__), 'output.png')
 output_img = Image.fromarray(output)
-output_img.save("output.png")
+output_img.save(myOutput)
 
 # works on small images fairly fast,,
 # be ready to NUKE! your system if you use a big picture, also helps to have a powerful GPU,
+# The script keeps working in an environment not where it is located.
+# Input gets worked on then the output is thrown into the oblivion
+# This temporary solution seems to fix it: the <os.path.join> thing
